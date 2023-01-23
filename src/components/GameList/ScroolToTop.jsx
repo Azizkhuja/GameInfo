7b31a0e7f838f181bd398b0ea6c10fe3558700dc
Aziz <aziz.khadjayev070@gmail.com>
2023-01-23 10:21:24 +0100

    Make it smooth scroll

--- src/components/GameList/ScroolToTop.jsx
+++ src/components/GameList/ScroolToTop.jsx
@@ -11,7 +11,12 @@ const ScroolToTop = () => {
       setVisible(false);
     }
   };
-  const scrollToTop = () => {};
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return <div>ScroolToTop</div>;
 };
 
