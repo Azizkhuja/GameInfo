08f0fedeb2d13b5955e14889ca54fdf82f4e23ed
Aziz <aziz.khadjayev070@gmail.com>
2023-01-23 10:20:43 +0100

    Check visiblity by scrool

--- src/components/GameList/ScroolToTop.jsx
+++ src/components/GameList/ScroolToTop.jsx
@@ -3,7 +3,14 @@ import { useState } from "react";
 const ScroolToTop = () => {
   const [visible, setVisible] = useState(false);
 
-  const toggleVisible = () => {};
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
   return <div>ScroolToTop</div>;
 };
 
