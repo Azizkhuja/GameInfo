c34d35eb7090ecab4e2d4f92beb806b4a6ce5cbf
Aziz <aziz.khadjayev070@gmail.com>
2023-01-26 07:55:36 +0100

    Create button wrapper

--- src/components/GameList/ScroolToTop.jsx
+++ src/components/GameList/ScroolToTop.jsx
@@ -32,4 +32,16 @@ const ScroolToTop = () => {
   );
 };
 
+export const Button = styled.div`
+  position: fixed;
+  width: 100%;
+  left: 50%;
+  bottom: 40px;
+  height: 20px;
+  font-size: 3rem;
+  z-index: 1;
+  cursor: pointer;
+  color: green;
+`;
+
 export default ScroolToTop;
